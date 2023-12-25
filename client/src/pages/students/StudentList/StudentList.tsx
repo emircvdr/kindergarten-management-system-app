450424226cd82e7d769b3b61b0f8153ec300f31b
Emir Çavdar <emircvdar@gmail.com>
2023-12-25 16:30:35 +0300

    .

--- client/src/pages/students/StudentList/StudentList.tsx
+++ client/src/pages/students/StudentList/StudentList.tsx
@@ -18,111 +18,111 @@ const Edit = styled.div`
   }
 `;
 const columns: GridColDef[] = [
-    {
-        field: "firstName",
-        headerName: "Adı",
-        minWidth: 150,
-        flex: 1,
-        editable: false,
+  {
+    field: "firstName",
+    headerName: "Adı",
+    minWidth: 150,
+    flex: 1,
+    editable: false,
+  },
+  {
+    field: "lastName",
+    headerName: "Soyadı",
+    minWidth: 150,
+    flex: 1,
+    editable: false,
+  },
+  {
+    field: "birthDate",
+    headerName: "Doğum Tarihi",
+    minWidth: 150,
+    flex: 1,
+    editable: false,
+  },
+  {
+    field: "id",
+    headerName: "İşlemler",
+    minWidth: 90,
+    width: 90,
+    renderCell: (params: any) => {
+      return <Edit>Düzenle</Edit>;
     },
-    {
-        field: "lastName",
-        headerName: "Soyadı",
-        minWidth: 150,
-        flex: 1,
-        editable: false,
-    },
-    {
-        field: "birthDate",
-        headerName: "Doğum Tarihi",
-        minWidth: 150,
-        flex: 1,
-        editable: false,
-    },
-    {
-        field: "id",
-        headerName: "İşlemler",
-        minWidth: 90,
-        width: 90,
-        renderCell: (params: any) => {
-            return <Edit>Düzenle</Edit>;
-        },
-    },
+  },
 ];
 
 const StudentList = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [rows, setRows] = React.useState<any[]>([]);
+  const [rows, setRows] = React.useState<any[]>([]);
 
-    React.useEffect(() => {
-        KindergartenAPI.GetStudents().then((res) => {
-            setRows(res.map((item: any) => ({ ...item, id: item._id })));
-        })
-    }, []);
+  React.useEffect(() => {
+    KindergartenAPI.GetStudents().then((res) => {
+      setRows(res.map((item: any) => ({ ...item, id: item._id })));
+    });
+  }, []);
 
-    return (
-        <Content
-            titleName="Öğrenciler"
-            header="Öğrenci Listesi"
-            content={
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        padding: "10px",
-                        gap: "20px",
-                    }}
-                >
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            gap: "10px",
-                            alignItems: "center",
-                        }}
-                    >
-                        <div style={{ width: "80%" }}>
-                            <TextField
-                                id="outlined-basic"
-                                label="Öğrenci Ara"
-                                variant="outlined"
-                                fullWidth
-                                size="small"
-                            />
-                        </div>
-                        <div style={{ width: "20%" }}>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                size="small"
-                                fullWidth
-                                onClick={() => navigate("/students/add")}
-                            >
-                                Yeni Öğrenci Ekle
-                            </Button>
-                        </div>
-                    </div>
-                    <DataGridContainer>
-                        <StyledDataGrid
-                            rows={rows}
-                            columns={columns}
-                            initialState={{
-                                pagination: {
-                                    paginationModel: {
-                                        pageSize: 5,
-                                    },
-                                },
-                            }}
-                            pageSizeOptions={[5]}
-                            disableRowSelectionOnClick
-                            disableColumnMenu
-                        />
-                    </DataGridContainer>
-                </div>
-            }
-        />
-    );
+  return (
+    <Content
+      titleName="Öğrenciler"
+      header="Öğrenci Listesi"
+      content={
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "10px",
+            gap: "20px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "10px",
+              alignItems: "center",
+            }}
+          >
+            <div style={{ width: "80%" }}>
+              <TextField
+                id="outlined-basic"
+                label="Öğrenci Ara"
+                variant="outlined"
+                fullWidth
+                size="small"
+              />
+            </div>
+            <div style={{ width: "20%" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                fullWidth
+                onClick={() => navigate("/students/add")}
+              >
+                Yeni Öğrenci Ekle
+              </Button>
+            </div>
+          </div>
+          <DataGridContainer>
+            <StyledDataGrid
+              rows={rows}
+              columns={columns}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 50,
+                  },
+                },
+              }}
+              pageSizeOptions={[5]}
+              disableRowSelectionOnClick
+              disableColumnMenu
+            />
+          </DataGridContainer>
+        </div>
+      }
+    />
+  );
 };
 
 export default StudentList;
