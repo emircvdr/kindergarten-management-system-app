9cff6c9e87bceec0418310a1edb0cde834784d9b
Emir Çavdar <emircvdar@gmail.com>
2024-01-24 15:46:29 +0300

    .

--- client/src/services/configService.ts
+++ client/src/services/configService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-const baseURL = "https://kindergarten-management-system-service.onrender.com";
-// const baseURL = "http://localhost:5001/";
+// const baseURL = "https://kindergarten-management-system-service.onrender.com";
+const baseURL = "http://localhost:5001/";
 export class ConfigApi {
   public static LibraryApi() {
     return axios.create({
